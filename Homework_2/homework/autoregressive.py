6fda508e46a786e112d81294d8a1e51923e8decf
PaulMeis022299 <122332762+PaulMeis022299@users.noreply.github.com>
2025-10-16 18:50:21 -0500

    Add files via upload

--- Homework_2/homework/autoregressive.py
+++ Homework_2/homework/autoregressive.py
@@ -57,6 +57,7 @@ class AutoregressiveModel(torch.nn.Module, Autoregressive):
         super().__init__()
         self.n_tokens = n_tokens
         self.token_embedding = torch.nn.Embedding(n_tokens, d_latent)
+        self.d_latent = d_latent
 
         # trnsformer layers for decoder
         self.layers = torch.nn.ModuleList([
