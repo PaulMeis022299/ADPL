c7b45e33ae36db32007ddda4ff95424ea892b792
PaulMeis022299 <122332762+PaulMeis022299@users.noreply.github.com>
2025-11-02 07:49:22 -0600

    Add files via upload

--- Homework_3/homework/rft.py
+++ Homework_3/homework/rft.py
@@ -1,5 +1,12 @@
 from .base_llm import BaseLLM
 from .sft import test_model
+from .sft import tokenize, format_example
+from .data import Dataset, benchmark
+
+from pathlib import Path
+from peft import get_peft_model, LoraConfig, PeftModel
+from transformers import Trainer, TrainingArguments
+import torch
 
 
 def load() -> BaseLLM:
@@ -17,12 +24,89 @@ def load() -> BaseLLM:
     return llm
 
 
+def format_example(prompt: str, answer: str, reasoning: str) -> dict[str, str]:
+    """
+    Construct a question / answerm + reasoning pair
+    """
+    answer_with_reasoning = reasoning.strip()
+    return {"question": prompt.strip(), "answer": answer_with_reasoning}
+
+
+class TokenizedDataset:
+    def __init__(self, tokenizer, rft_list: list[tuple], format_fn):
+        """
+        rft_list: list of [question:str, float_answer:float, reasoning_with_answer:str]
+        format_fn: function(q, float_answer, reasoning_with_answer) -> {'question': str, 'answer': str}
+        """
+        self.format_fn = format_fn
+        self.tokenizer = tokenizer
+        self.data = rft_list
+
+    def __len__(self):
+        return len(self.data)
+
+    def __getitem__(self, idx):
+        q, true_ans, reasoning = self.data[idx]
+        formatted = self.format_fn(q, true_ans, reasoning)
+        return tokenize(self.tokenizer, formatted["question"], formatted["answer"])
+
+
+
+
 def train_model(
     output_dir: str,
     **kwargs,
 ):
-    # Reuse much of the SFT code here
-    raise NotImplementedError()
+   
+    output_dir = r"rft_model"
+    base = BaseLLM()
+    model = base.model
+    tokenizer = base.tokenizer
+
+    lora_config = LoraConfig(
+        r=16,                        
+        lora_alpha=64,              
+        target_modules="all-linear",
+        bias="none",
+        task_type="CAUSAL_LM",
+    )
+    model = get_peft_model(model, lora_config)
+    model.train()
+
+    if torch.cuda.is_available():
+        model.enable_input_require_grads()
+
+    train_data = Dataset("rft")
+
+    train_dataset = TokenizedDataset(tokenizer, train_data, format_example)
+
+    args = TrainingArguments(
+        output_dir=output_dir,
+        logging_dir=output_dir,
+        report_to="tensorboard",
+        num_train_epochs=5,
+        per_device_train_batch_size=32,
+        gradient_checkpointing=True,
+        learning_rate=1e-3,
+        save_strategy="epoch",
+        save_total_limit= 2,
+        logging_steps=10,
+    )
+
+    trainer = Trainer(
+        model=model,
+        args=args,
+        train_dataset=train_dataset,
+        tokenizer=tokenizer,
+    )
+
+    
+    trainer.train()
+
+    Path(output_dir).mkdir(parents=True, exist_ok=True)
+    model.save_pretrained(output_dir)
+
+    test_model(output_dir)
 
 
 if __name__ == "__main__":
